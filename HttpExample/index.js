a476f50b91fd5839da24c1af8cdc0be931c1ae78
SumathyV <sumathy.palaniswamy@gmail.com>
2023-09-15 19:30:30 +0530

    Test- REST API call

--- HttpExample/index.js
+++ HttpExample/index.js
@@ -1,13 +1,45 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+const https = require("https");
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
+module.exports = async function (context, req) {
+  context.log("JavaScript HTTP trigger function processed a request.");
+
+  const name = req.query.name || (req.body && req.body.name);
+  /* const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-}
+  //body: responseMessage;
+  //};
+
+  //Invoke REST API
+
+  https.get("https://jsonplaceholder.typicode.com/users", (res) => {
+      let data = [];
+      const headerDate =
+      res.headers && res.headers.date ? res.headers.date : "no response date";
+      console.log("Status Code:", res.statusCode);
+      console.log("Date in Response header:", headerDate);
+
+      res.on("data", (chunk) => {
+        data.push(chunk);
+      });
+
+      res.on("end", () => {
+        console.log("Response ended: ");
+        const users = JSON.parse(Buffer.concat(data).toString());
+
+        for (user of users) {
+          console.log(`Got user with id: ${user.id}, name: ${user.name}`);
+        }
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: data
+      };
+      });
+    })
+    .on("error", (err) => {
+      console.log("Error: ", err.message);
+    });
+};
