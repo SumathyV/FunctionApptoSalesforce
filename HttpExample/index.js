adcad5edc01ff5e7f6661eafd6e01525925fd158
SumathyV <sumathy.palaniswamy@gmail.com>
2023-09-15 20:03:58 +0530

    Test- REST API call

--- HttpExample/index.js
+++ HttpExample/index.js
@@ -36,7 +36,7 @@ module.exports = async function (context, req) {
         }
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: Buffer.concat(data).toString()
+            body:"Sumathy"
       };
       });
     })
