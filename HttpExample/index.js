1ec73ed88644ac5e10ebc53caddf4ddd55ffeebb
SumathyV <sumathy.palaniswamy@gmail.com>
2023-09-15 21:26:35 +0530

    Test- REST API call

--- HttpExample/index.js
+++ HttpExample/index.js
@@ -19,23 +19,26 @@ module.exports = async function (context, req) {
       let data = [];
       const headerDate =
       res.headers && res.headers.date ? res.headers.date : "no response date";
-      console.log("Status Code:", res.statusCode);
-      console.log("Date in Response header:", headerDate);
+      context.log("Status Code:", res.statusCode);
+      context.log("Date in Response header:", headerDate);
 
       res.on("data", (chunk) => {
         data.push(chunk);
       });
 
       res.on("end", () => {
-        console.log("Response ended: ");
+        context.log("Response ended: ");
         const users = JSON.parse(Buffer.concat(data).toString());
 
         for (user of users) {
-          console.log(`Got user with id: ${user.id}, name: ${user.name}`);
+            context.log(`Got user with id: ${user.id}, name: ${user.name}`);
         }
         context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: data
+            status: 200, /* Defaults to 200 */
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: users
       };
       });
     })
