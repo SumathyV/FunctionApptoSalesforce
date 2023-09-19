ca7199ba3444258810e60029cbbb67ee7e0dfafa
SumathyV <sumathy.palaniswamy@gmail.com>
2023-09-19 17:40:24 +0530

    Test- REST API call

--- HttpExample/index.js
+++ HttpExample/index.js
@@ -1,5 +1,5 @@
 const https = require("https");
-console.log("Calling using https")
+console.log("Calling using https");
 module.exports = async function (context, req) {
   context.log("JavaScript HTTP trigger function processed a request.");
 
@@ -14,33 +14,33 @@ module.exports = async function (context, req) {
   //};
 
   //Invoke REST API
-  context.log("STEP 1",name);
+  context.log("STEP 1", name);
 
-  https.get("https://jsonplaceholder.typicode.com/users", function (res) {
-      let data = [];
-      const headerDate =
-      res.headers && res.headers.date ? res.headers.date : "no response date";
-      context.log("Status Code:", res.statusCode);
-      context.log("Date in Response header:", headerDate);
+  let res = await https.get("https://jsonplaceholder.typicode.com/users");
+  context.log("res==>", res);
+  let data = [];
+  const headerDate =
+    res.headers && res.headers.date ? res.headers.date : "no response date";
+  context.log("Status Code:", res.statusCode);
+  context.log("Date in Response header:", headerDate);
 
-      res.on("data", (chunk) => {
-        data.push(chunk);
-      });
+  res.on("data", (chunk) => {
+    data.push(chunk);
+  });
 
-      res.on("end", () => {
-        context.log("Response ended: ");
-        const users = JSON.parse(Buffer.concat(data).toString());
+  res.on("end", () => {
+    context.log("Response ended: ");
+    const users = JSON.parse(Buffer.concat(data).toString());
 
-        for (user of users) {
-            context.log(`Got user with id: ${user.id}, name: ${user.name}`);
-        }
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: data
-      };
-      });
-    })
-    .on("error", (err) => {
-      context.log("Error: ", err.message);
-    });
+    for (user of users) {
+      context.log(`Got user with id: ${user.id}, name: ${user.name}`);
+    }
+    context.res = {
+      // status: 200, /* Defaults to 200 */
+      body: data,
+    };
+  });
+  res.on("error", (err) => {
+    context.log("Error: ", err.message);
+  });
 };
